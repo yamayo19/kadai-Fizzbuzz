e9a4a39ec2706dd5eb26aaa899c7f3a26fbb2984
yosuke.yamamoto <ymmtysk19@gmail.com>
2021-08-22 04:26:53 +0000

    kadai-Fizzbuzz-resubmit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,14 +1,16 @@
-def fizzbuzz(a,b)
-  (a..b).each do |number|
+def fizzbuzz(num)
+  number = num
       if (number%3 == 0)&&(number%5 == 0) 
-          puts "fizzbuzz"
+          puts "FizzBuzz"
       elsif number%3 == 0
           puts 'Fizz'
       elsif number%5 == 0
           puts "Buzz"
       else 
           puts number
-    end
 end
 end
-fizzbuzz(1,100)
+fizzbuzz(1)
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
