0b069b13b3e03f246639d717294f491623a78f98
yosuke.yamamoto <ymmtysk19@gmail.com>
2021-08-22 07:01:31 +0000

    kadai-fizzbuzz-submit-third

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,16 +1,16 @@
 def fizzbuzz(num)
   number = num
-      if (number%3 == 0)&&(number%5 == 0) 
-          puts "FizzBuzz"
-      elsif number%3 == 0
-          puts 'Fizz'
-      elsif number%5 == 0
-          puts "Buzz"
-      else 
-          puts number
+  if (number%3 == 0)&&(number%5 == 0) 
+    "FizzBuzz"
+  elsif number%3 == 0
+    'Fizz'
+  elsif number%5 == 0
+    "Buzz"
+  else 
+    number
+  end
 end
+num_max = 100
+(1..num_max).each do |number|
+  puts fizzbuzz(number)
 end
-fizzbuzz(1)
-fizzbuzz(3)
-fizzbuzz(5)
-fizzbuzz(15)
